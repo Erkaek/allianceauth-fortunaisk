86f409530ca2da70121b9cbf13698817ebb003eb
erkaek <erkaekanon@outlook.com>
2024-12-14 18:34:23 +0100

    update

--- fortunaisk/apps.py
+++ fortunaisk/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 class FortunaiskConfig(AppConfig):
     name = 'fortunaisk'
     verbose_name = "Fortuna ISK Raffle"
-
-    def ready(self):
-        import fortunaisk.signals  # Import des signaux si nécessaire
