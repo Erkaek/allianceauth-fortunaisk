d77e2b901a8d34bbdf9f127dc4339c0c323f5b79
erkaek <erkaekanon@outlook.com>
2024-12-14 19:14:21 +0100

    .

--- fortunaisk/urls.py
+++ fortunaisk/urls.py
@@ -1,30 +1,28 @@
-import logging
 from django.urls import path
 from allianceauth.services.hooks import MenuItemHook
 from django.utils.translation import gettext_lazy as _
 from . import views
 
-# Configuration du logger
-logger = logging.getLogger(__name__)
+app_name = 'fortunaisk'
 
+# Hook pour ajouter le module au menu principal d'Alliance Auth
 class RaffleMenu(MenuItemHook):
     def __init__(self):
-        logger.debug("[DEBUG] Enregistrement du hook RaffleMenu.")
         super().__init__(
-            _('Raffle'),
-            'fortunaisk:main_view',
-            navactive=['fortunaisk']
+            _('FortunaISK'),  # Nom affiché dans le menu
+            'fortunaisk:main_view',  # Nom de la vue principale
+            navactive=['fortunaisk']  # Active l'entrée pour toutes les pages du module
         )
 
     def permissions(self, user):
-        logger.debug(f"[DEBUG] Vérification des permissions pour : {user.username}")
+        # Définir la permission requise pour afficher le module
         return user.has_perm('fortunaisk.view_raffle')
 
+# Enregistrement du Hook
 MenuItemHook.register(RaffleMenu)
 
-app_name = 'fortunaisk'
-
+# Définition des URLs du module
 urlpatterns = [
-    path('', views.main_view, name='main_view'),
-    path('history/', views.history_view, name='history_view'),
+    path('', views.main_view, name='main_view'),  # Vue principale
+    path('history/', views.history_view, name='history_view'),  # Vue historique des gagnants
 ]
