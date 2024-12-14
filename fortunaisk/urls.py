1686cf563ab9d0ad7989a2c87f217cfd44af7756
erkaek <erkaekanon@outlook.com>
2024-12-14 20:00:36 +0100

    autre

--- fortunaisk/urls.py
+++ fortunaisk/urls.py
@@ -1,29 +1,24 @@
-from django.urls import path
 from allianceauth.services.hooks import MenuItemHook
 from django.utils.translation import gettext_lazy as _
+from django.urls import path
 from . import views
-import logging
-
-logger = logging.getLogger(__name__)
 
 app_name = 'fortunaisk'
 
-# Hook pour le menu principal
+# Hook pour ajouter l'entrée dans le menu principal
 class RaffleMenu(MenuItemHook):
     def __init__(self):
-        logger.debug("[DEBUG] Hook RaffleMenu chargé pour FortunaISK.")
         super().__init__(
-            _('FortunaISK'),
-            'fortunaisk:main_view',
+            _('FortunaISK'),  # Texte affiché dans le menu
+            'fortunaisk:main_view',  # Vue principale
             navactive=['fortunaisk']
         )
 
     def permissions(self, user):
-        return user.has_perm('fortunaisk.view_raffle')
+        return user.has_perm('fortunaisk.view_raffle')  # Vérifiez les permissions
 
 MenuItemHook.register(RaffleMenu)
 
-# Routes du module
 urlpatterns = [
     path('', views.main_view, name='main_view'),
     path('history/', views.history_view, name='history_view'),
