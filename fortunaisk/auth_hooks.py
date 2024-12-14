aa4d21675eb3b4efe1e3eec5a13262fc2901ade6
erkaek <erkaekanon@outlook.com>
2024-12-15 00:52:58 +0100

    a

--- fortunaisk/auth_hooks.py
+++ fortunaisk/auth_hooks.py
@@ -1,19 +1,30 @@
 # fortunaisk/auth_hooks.py
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from allianceauth.hooks import MenuItemHook, UrlHook
-from django.urls import include, path, reverse
+from . import urls
+
+def provides_app_settings():
+    return {
+        'name': 'FortunaISK',
+        'version': '0.1.0',
+        'description': _('A plugin to view and manage ISK fortunes'),
+        'author': 'Votre Nom',
+    }
+
+def load():
+    # Aucune insertion de menu direct ici, c'est géré par les décorateurs
+    pass
 
 @UrlHook()
-def fortunaisk_urls():
-    # Inclut les URLs de l’app fortunaisk
-    return path('fortunaisk/', include('fortunaisk.urls', namespace='fortunaisk'))
+def register_urls():
+    return urls, 'fortunaisk', 'fortunaisk'
 
 @MenuItemHook()
-def fortunaisk_menu_item(request):
+def register_menu():
     return {
         'name': _('FortunaISK'),
-        'url': reverse('fortunaisk:index'),
         'icon': 'fas fa-coins',
-        'order': 100,
-        'groups': {'all': True}  # accessible à tous les utilisateurs connectés
+        'url': reverse('fortunaisk:index'),
+        'groups': {'all': True},
     }
