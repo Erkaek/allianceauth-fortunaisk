a982c13be251d936cb99bd3f795112d753dd9374
erkaek <erkaekanon@outlook.com>
2024-12-15 00:53:42 +0100

    a

--- fortunaisk/auth_hooks.py
+++ fortunaisk/auth_hooks.py
@@ -1,30 +1,35 @@
 # fortunaisk/auth_hooks.py
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from allianceauth.hooks import MenuItemHook, UrlHook
-from . import urls
+from allianceauth.hooks import register_hook
+
+__version__ = '0.1.0'
+__title__ = 'FortunaISK'
 
 def provides_app_settings():
     return {
-        'name': 'FortunaISK',
-        'version': '0.1.0',
+        'name': __title__,
+        'version': __version__,
         'description': _('A plugin to view and manage ISK fortunes'),
         'author': 'Votre Nom',
     }
 
 def load():
-    # Aucune insertion de menu direct ici, c'est géré par les décorateurs
+    # Aucun import de MenuItemHook ou UrlHook ici
+    # Pas de code d'enregistrement direct du menu ou des URLs ici
     pass
 
-@UrlHook()
-def register_urls():
-    return urls, 'fortunaisk', 'fortunaisk'
+@register_hook('url_hook')
+def fortunaisk_urls():
+    # On indique à AllianceAuth quel module d’URLs utiliser, ainsi que le namespace et l’app_name
+    return ('fortunaisk.urls', 'fortunaisk', 'fortunaisk')
 
-@MenuItemHook()
-def register_menu():
+@register_hook('menu_item_hook')
+def fortunaisk_menu_item():
+    # On ajoute un item de menu pour le dashboard
     return {
         'name': _('FortunaISK'),
-        'icon': 'fas fa-coins',
+        'icon': 'fas fa-coins',  # Une icône FontAwesome
         'url': reverse('fortunaisk:index'),
-        'groups': {'all': True},
+        'groups': {'all': True},  # Visible par tous les utilisateurs authentifiés
     }
