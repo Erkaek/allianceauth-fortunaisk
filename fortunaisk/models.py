56dc28d83277b2bdd23e81eb9b765361e913355e
erkaek <erkaekanon@outlook.com>
2024-12-14 18:31:49 +0100

    update models

--- fortunaisk/models.py
+++ fortunaisk/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-from allianceauth.characters.models import Character  # Import du modèle Character
+from allianceauth.eveonline.models import EveCharacter as Character
+
 
 class RaffleTicket(models.Model):
     """
@@ -22,7 +23,17 @@ class RaffleTicket(models.Model):
     payment_reference = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return f"Ticket #{self.pk} - {self.user.username} - {self.character.name} - {self.price_isk} ISK"
+        return f"Ticket #{self.pk} - {self.user.username} - {self.character.character_name} - {self.price_isk} ISK"
+
+    @property
+    def is_valid(self):
+        """
+        Vérifie si le ticket est valide (par exemple, vérification de la date ou du paiement).
+        """
+        # Exemple de validation : Si la date d'achat dépasse un mois, invalider
+        one_month_ago = timezone.now() - timezone.timedelta(days=30)
+        return self.purchase_date >= one_month_ago
+
 
 class RaffleWinner(models.Model):
     """
@@ -41,3 +52,10 @@ class RaffleWinner(models.Model):
     def __str__(self):
         date_str = self.draw_date.strftime('%Y-%m-%d %H:%M')
         return f"Gagnant du {date_str} - {self.user.username if self.user else 'Inconnu'}"
+
+    @classmethod
+    def last_winner(cls):
+        """
+        Retourne le dernier gagnant enregistré.
+        """
+        return cls.objects.order_by('-draw_date').first()
