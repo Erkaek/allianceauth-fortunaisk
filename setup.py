ad5f1e95f1cd3f8b4720bcd1ce3e64057b864cc4
erkaek <erkaekanon@outlook.com>
2024-12-14 19:57:53 +0100

    autre

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='allianceauth-fortunaisk',
+    name='fortunaisk',  # Assurez-vous que le nom ici est "fortunaisk"
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'allianceauth>=4.0',
     ],
+    entry_points='''
+        [console_scripts]
+        fortunaisk=fortunaisk:main
+    ''',
 )
