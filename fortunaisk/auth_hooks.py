97b10f31dcee1359b0ce7115f8fc4cc5eaca729b
erkaek <erkaekanon@outlook.com>
2024-12-14 20:15:52 +0100

    autre

---
+++ fortunaisk/auth_hooks.py
@@ -0,0 +1,48 @@
+"""Hook into Alliance Auth"""
+
+# Django
+from django.utils.translation import gettext_lazy as _
+
+# Alliance Auth
+from allianceauth import hooks
+from allianceauth.services.hooks import MenuItemHook, UrlHook
+
+# FortunaISK
+from fortunaisk import urls
+
+
+class FortunaISKMenuItem(MenuItemHook):
+    """Menu item for FortunaISK"""
+
+    def __init__(self):
+        # Set up menu entry for sidebar
+        MenuItemHook.__init__(
+            self,
+            _("FortunaISK"),  # Display name
+            "fas fa-ticket-alt fa-fw",  # Font Awesome icon
+            "fortunaisk:main_view",  # URL name
+            navactive=["fortunaisk:"],  # Highlight menu when active
+        )
+
+    def render(self, request):
+        """Render the menu item"""
+
+        # Check if the user has the necessary permissions
+        if request.user.has_perm("fortunaisk.view_raffle"):
+            return MenuItemHook.render(self, request)
+
+        return ""  # Return empty if the user lacks permissions
+
+
+@hooks.register("menu_item_hook")
+def register_menu():
+    """Register the menu item for FortunaISK"""
+
+    return FortunaISKMenuItem()
+
+
+@hooks.register("url_hook")
+def register_urls():
+    """Register app URLs"""
+
+    return UrlHook(urls, "fortunaisk", r"^fortunaisk/")
