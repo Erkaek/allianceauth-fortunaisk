0c1c48fe94d0ee72546c79fec260b7a2ea2f5e88
erkaek <erkaekanon@outlook.com>
2024-12-15 01:28:09 +0100

    autre

--- fortunaisk/auth_hooks.py
+++ fortunaisk/auth_hooks.py
@@ -1,28 +1,35 @@
+# fortunaisk/auth_hooks.py
+
 from allianceauth import hooks
-from allianceauth.services.hooks import UrlHook, MenuItemHook
+from allianceauth.services.hooks import MenuItemHook, UrlHook
+from . import urls
 
-class FortunaISKUrls(UrlHook):
-    def __init__(self):
-        from . import urls
-        super().__init__((urls.urlpatterns, 'fortunaisk'), 'fortunaisk', 'fortunaisk/')
-
-# Important: 
-# Assurez-vous que le UrlHook est enregistré avant le MenuItemHook
-
+# D'abord, on enregistre les URLs via un UrlHook en suivant le même principe que corptools
 @hooks.register('url_hook')
-def register_urls():
-    return FortunaISKUrls()
+def register_url():
+    # Ici, on inclut les urls du module fortunaisk sous le namespace 'fortunaisk'
+    # et on leur donne un préfixe d'URL. Par exemple '^fortunaisk/' pour préfixer les urls.
+    return UrlHook(urls, 'fortunaisk', r'^fortunaisk/')
 
+# Ensuite, on crée la classe pour le menu
 class FortunaISKMenu(MenuItemHook):
     def __init__(self):
-        # Arguments positionnels: texte, url_name, icône, etc.
-        super().__init__(
-            'FortunaISK',               # Le texte affiché dans le menu
-            'fortunaisk:current_lottery', # Le nom du pattern d'URL
-            'fa fa-ticket',             # L'icône Font Awesome
+        # Selon l'exemple corptools :
+        # MemberAudit: MenuItemHook.__init__(self, app_settings.CORPTOOLS_APP_NAME, 'far fa-eye fa-fw', 'corptools:react', ...)
+        # On suit donc le même pattern : (text, icon, url_name, ...)
+        #
+        # text = "FortunaISK"
+        # icon = "fa fa-ticket"
+        # url_name = "fortunaisk:current_lottery"
+        #
+        # navactive = ['fortunaisk:'] permet de marquer l'item actif sur les pages fortunaisk
+        MenuItemHook.__init__(
+            self,
+            'FortunaISK',                 # texte affiché
+            'fa fa-ticket',               # icône Font Awesome
+            'fortunaisk:current_lottery', # nom du pattern d'URL
             navactive=['fortunaisk:']
         )
-
 
 @hooks.register('menu_item_hook')
 def register_menu():
--- fortunaisk/urls.py
+++ fortunaisk/urls.py
@@ -1,3 +1,4 @@
+# fortunaisk/urls.py
 from django.urls import path
 from . import views
 
