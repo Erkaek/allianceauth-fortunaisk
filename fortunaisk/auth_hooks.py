f4b1152a0d30f57b27f06199972411c23da4219e
erkaek <erkaekanon@outlook.com>
2024-12-15 01:24:09 +0100

    autre

--- fortunaisk/auth_hooks.py
+++ fortunaisk/auth_hooks.py
@@ -4,26 +4,26 @@ from allianceauth.services.hooks import UrlHook, MenuItemHook
 class FortunaISKUrls(UrlHook):
     def __init__(self):
         from . import urls
-        # On passe (urlpatterns, app_name), puis namespace, puis base_url
         super().__init__((urls.urlpatterns, 'fortunaisk'), 'fortunaisk', 'fortunaisk/')
 
-class FortunaISKMenu(MenuItemHook):
-    def __init__(self):
-        # On passe les arguments positionnellement :
-        # 1er: text
-        # 2ème: url_name
-        # 3ème: icon (optionnel)
-        # navactive en argument nommé est permis
-        super().__init__(
-            'FortunaISK',               # text
-            'fortunaisk:current_lottery',  # url_name (nom du pattern d'URL, pas reverse())
-            'fa fa-ticket',             # icon en 3ème argument, sans nom
-            navactive=['fortunaisk:']   # argument nommé autorisé
-        )
+# Important: 
+# Assurez-vous que le UrlHook est enregistré avant le MenuItemHook
 
 @hooks.register('url_hook')
 def register_urls():
     return FortunaISKUrls()
+
+class FortunaISKMenu(MenuItemHook):
+    def __init__(self):
+        # text = 'FortunaISK'
+        # url_name = 'fortunaisk:current_lottery'
+        # icon = 'fas fa-ticket' (solide, cohérent avec FA5+)
+        super().__init__(
+            'FortunaISK',
+            'fortunaisk:current_lottery',
+            'fas fa-ticket',
+            navactive=['fortunaisk:']
+        )
 
 @hooks.register('menu_item_hook')
 def register_menu():
