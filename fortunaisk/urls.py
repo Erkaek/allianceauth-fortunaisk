4f98af8d485f8d4f623e6614709069cf714587f4
erkaek <erkaekanon@outlook.com>
2024-12-15 01:02:54 +0100

    a

--- fortunaisk/urls.py
+++ fortunaisk/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'fortunaisk'
@@ -6,4 +6,5 @@ app_name = 'fortunaisk'
 urlpatterns = [
     path('', views.current_lottery, name='current_lottery'),
     path('winners/', views.winners_history, name='winners_history'),
+    path('fortunaisk/', include('fortunaisk.urls', namespace='fortunaisk')),
 ]
