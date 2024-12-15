b5daf97d61f75ecc782000a659ddfd9a47534efa
erkaek <erkaekanon@outlook.com>
2024-12-16 00:26:02 +0100

    a

--- fortunaisk/urls.py
+++ fortunaisk/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'fortunaisk'
+app_name = "fortunaisk"
 
 urlpatterns = [
-    path('tickets/', views.tickets_list, name='tickets_list'),
-    path('winners/', views.winner_list, name='winner_list'),  # Ensure this matches 'winner_list'
-    path('admin/', views.admin_dashboard, name='admin'),
+    path("tickets/", views.tickets_list, name="tickets_list"),
+    path("winners/", views.winner_list, name="winner_list"),
+    path("admin/", views.admin_dashboard, name="admin"),
 ]
--- fortunaisk/views.py
+++ fortunaisk/views.py
@@ -2,26 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Ticket, Winner
 
-
 @login_required
 def tickets_list(request):
     tickets = Ticket.objects.filter(character__character_ownership__user=request.user)
-    return render(request, 'fortunaisk/tickets_list.html', {'tickets': tickets})
-
+    return render(request, "fortunaisk/tickets_list.html", {"tickets": tickets})
 
 @login_required
 def winner_list(request):
-    """View for displaying the list of winners."""
     winners = Winner.objects.all()
-    return render(request, 'fortunaisk/winners_list.html', {'winners': winners})
+    return render(request, "fortunaisk/winner_list.html", {"winners": winners})
 
-
-
-@permission_required('fortunaisk.admin')
+@permission_required("fortunaisk.admin")
 def admin_dashboard(request):
-    tickets = Ticket.objects.all()
-    winners = Winner.objects.all()
-    return render(request, 'fortunaisk/admin_dashboard.html', {
-        'tickets': tickets,
-        'winners': winners
-    })
+    return render(request, "fortunaisk/admin.html")
